7e41edc84ced18aa8527b2dccb372b83d57b2b22
Leonard Fuechsel <leonard.fuechsel@gmail.com>
2019-06-13 13:43:50 +0200

    extract actions

---
+++ src/app/rasa-core/rasa-core-action-score.ts
@@ -0,0 +1,4 @@
+export class RasaCoreActionScore{
+  action: string;
+  score: number;
+}
--- src/app/rasa-core/rasa-core.component.ts
+++ src/app/rasa-core/rasa-core.component.ts
@@ -5,6 +5,7 @@ import { RasaNluResponse } from '../rasa-nlu/rasa-nlu-response';
 import { RasaCoreQuery } from './rasa-core-query';
 import { RasaCoreMessage } from './rasa-core-message';
 import { RasaCoreAction } from './rasa-core-action';
+import { RasaCoreActionScore } from './rasa-core-action-score';
 
 @Component({
   selector: 'app-rasa-core',
@@ -15,9 +16,13 @@ export class RasaCoreComponent implements OnInit {
 
   text = 'Hello';
   messages = [];
+  action: string;
+  score: number;
+  actionScore = new RasaCoreActionScore();
   rasaCoreQuery = new RasaCoreQuery();
   rasaNluResponse = new RasaNluResponse();
   rasaCoreMessage = new RasaCoreMessage();
+  rasaCoreActionScores = [];
   rasaCoreAction = new RasaCoreAction();
 
   constructor(private ref: ChangeDetectorRef,
@@ -26,7 +31,58 @@ export class RasaCoreComponent implements OnInit {
   ngOnInit() {
   }
 
+  parseModel(text): void {
+    this.rasaCoreQuery.text = text;
+    this.rasaCoreService.parseModel(this.rasaCoreQuery).subscribe((rasaNluResponse: RasaNluResponse) => {
+      this.rasaNluResponse = rasaNluResponse;
+
+      this.addMessage(text, 'user', rasaNluResponse);
+    });
+  }
+
+  addMessage(text: string, sender: string, parse_data: RasaNluResponse): void {
+    this.rasaCoreMessage.text = text;
+    this.rasaCoreMessage.sender = sender;
+    this.rasaCoreMessage.parse_data = parse_data;
+
+    this.rasaCoreService.addMessage(this.rasaCoreMessage).subscribe((any) => {
+      this.predictAction();
+      this.ref.detectChanges();
+    });
+  }
+
+  predictAction(): void {
+    this.rasaCoreActionScores = [];
+
+    this.rasaCoreService.predictAction().subscribe((any) => {
+      this.rasaCoreActionScores = any.scores;
+      this.actionScore = this.rasaCoreActionScores[0];
+      this.action = this.actionScore.action;
+    });
+  }
+
+  setAction(): void {
+    this.actionScore = this.rasaCoreActionScores.find(actionScore => actionScore.action === this.action);
+  }
+
+  executeAction(): void {
+    this.rasaCoreAction.name = this.action;
+    this.rasaCoreAction.policy = 'string';
+    this.rasaCoreAction.confidence = this.actionScore.score;
+    console.log(this.rasaCoreAction);
+    this.rasaCoreService.executeAction(this.rasaCoreAction).subscribe((any) => {
+      if (any.messages[0]) {
+        this.messages.push('Bot: ' + any.messages[0].text);
+      }
+    });
+  }
+
   send(): void {
+    this.messages.push('User: ' + this.text);
+    this.parseModel(this.text);
+  }
+
+  chat(): void {
     // console.log(this.text);
     this.messages.push('User: ' + this.text);
 
@@ -44,6 +100,7 @@ export class RasaCoreComponent implements OnInit {
       this.rasaCoreService.addMessage(this.rasaCoreMessage).subscribe((any) => {
 
         this.rasaCoreService.predictAction().subscribe((any) => {
+          this.predictAction();
           this.rasaCoreAction.name = any.scores[0].action;
           if (this.rasaCoreAction.name === 'action_listen'){
             this.rasaCoreAction.name = any.scores[1].action;
@@ -53,15 +110,16 @@ export class RasaCoreComponent implements OnInit {
           console.log(this.rasaCoreAction);
           this.rasaCoreService.executeAction(this.rasaCoreAction).subscribe((any) => {
 
-            console.log(any);
-            this.messages.push('Bot: ' + any.messages[0].text);
+            if (any.messages[0].text) {
+              this.messages.push('Bot: ' + any.messages[0].text);
+            }
           });
         });
       });
     });
   }
 
-  restart(): void {
+  clear(): void {
     this.rasaCoreAction = new RasaCoreAction();
     this.rasaCoreAction.name = 'action_restart';
     this.rasaCoreService.executeAction(this.rasaCoreAction).subscribe((any) => {
