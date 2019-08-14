eae89bef060583c84cab80fdf07db3136433332e
Leonard Fuechsel <leonard.fuechsel@gmail.com>
2019-08-14 17:38:40 +0200

    load mock result

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit, ViewChild, ViewChildren, QueryList, AfterViewInit, ChangeDetectorRef } from '@angular/core';
-import { TestTurn } from '../test-turn/test-turn';
+
 import { TestResult } from '../test-result';
-import { TIME_NEXT_CRITERIA, TIME_CITY_END_CRITERIA } from '../mock-test-criteria';
-import { TIME_RESULT, RIGHT_RESULT, WRONG_RESULT } from '../mock-result';
-import { Result } from '../result/result';
-import { TIME_NEXT_TURN } from '../mock-test-turn';
 import { ResultComponent } from '../result/result.component';
-import { Criteria } from '../criteria/criteria';
-import { TIME_LISSABON_GOOLGE, TIME_GOOLGE, TIME_LISSABON_ALEXA, TIME_ALEXA } from '../mock-test-case';
-import { TestCase } from '../test-case';
+
 import { BackendService } from '../backend.service';
 import { TEST_TIME_DIALOG_GOOGLE, TEST_TIME_DIALOG_ALEXA } from '../mock-test-result';
 
@@ -17,30 +11,19 @@ import { TEST_TIME_DIALOG_GOOGLE, TEST_TIME_DIALOG_ALEXA } from '../mock-test-re
   templateUrl: './review.component.html',
   styleUrls: ['./review.component.css']
 })
-export class ReviewComponent implements OnInit, AfterViewInit {
-
-  @ViewChild(ResultComponent)
-  private resultComponent: ResultComponent;
+export class ReviewComponent implements OnInit {
 
   @ViewChildren(ResultComponent)
   private resultComponents: QueryList<ResultComponent>;
 
-  testResults: TestResult[] = [TEST_TIME_DIALOG_GOOGLE, TEST_TIME_DIALOG_ALEXA];
-  testResult: TestResult = this.testResults[0];
+  testResults: TestResult[] = [];
+  testResult: TestResult;
 
   constructor(private ref: ChangeDetectorRef,
               private backendService: BackendService) { }
 
   ngOnInit() {
-    // this.loadResults();
-    // this.getResult('5d52a499142f04fb86095db5');
-
-  }
-
-  ngAfterViewInit(): void {
-    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    //Add 'implements AfterViewInit' to the class.
-    this.showResult();
+    this.loadMockResults();
   }
 
   changeTestResult(): void {
@@ -57,8 +40,17 @@ export class ReviewComponent implements OnInit, AfterViewInit {
   loadResults(): void {
     this.backendService.getResults().subscribe((testResults: TestResult[]) => {
       this.testResults = testResults;
-      console.log(this.testResults);
+      this.testResult = this.testResults[0];
+      this.ref.detectChanges();
+      this.showResult();
     });
+  }
+
+  loadMockResults(): void {
+    this.testResults = [TEST_TIME_DIALOG_GOOGLE, TEST_TIME_DIALOG_ALEXA];
+    this.testResult = this.testResults[0];
+    this.ref.detectChanges();
+    this.showResult();
   }
 
   async getResult(id) {
@@ -69,7 +61,6 @@ export class ReviewComponent implements OnInit, AfterViewInit {
       console.log('Error: ' + error);
     }
   }
-
 
   // save dialog
   async saveResult() {
