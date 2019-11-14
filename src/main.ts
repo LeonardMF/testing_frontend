dce5730f378aeee41399756a4a67d4cd5d223722
Leonard Fuechsel <leonard.fuechsel@gmail.com>
2019-11-14 10:11:20 +0100

    add ms cloud

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,26 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { MicrosoftModule } from 'speech-angular';
+
+import { MICROSOFT_REGION, MICROSOFT_SUBSCRIPTION_KEY } from './../credentials/microsoft-credentials-leo';
+
+// Microsoft-Credentials
+const microsoftOption = {
+  microsoftDynamicCredentialsFlag: false,
+  microsoftRegion: MICROSOFT_REGION,
+  microsoftSubscriptionKey: MICROSOFT_SUBSCRIPTION_KEY,
+  errorOutputFlag: false
+};
+
+// Initialisierung des Microsoft Cloud-Service
+MicrosoftModule.init( microsoftOption, (aMicrosoftFlag: boolean) => {
+  if ( microsoftOption && microsoftOption.errorOutputFlag ) {
+      console.log( '===> Microsoft:', aMicrosoftFlag);
+  }
+  environment.microsoft = aMicrosoftFlag;
+});
+
 if (environment.production) {
   enableProdMode();
 }
