b6d03e00851689cd7f38ff0d14b49ac61be0443a
Leonard Fuechsel <leonard.fuechsel@gmail.com>
2019-11-14 09:51:45 +0100

    settings for tts and asr

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { SpeakService, ListenService } from 'speech-angular';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  language: string;
+  asr: string;
+  tts: string;
+  voice: string;
+  voiceList: string[];
+
+  constructor( private speakService: SpeakService,
+               private listenService: ListenService,
+               private ref: ChangeDetectorRef) { }
 
   ngOnInit() {
+    this.language = 'de';
+    this.setLanguage();
+    this.asr = this.listenService.asr;
+    this.tts = this.speakService.tts;
+    this.voice = this.speakService.voice;
+    this.voiceList = this.speakService.getVoiceList();
+  }
+
+  getVoice(): void {
+    this.voice = '';
+    this.voiceList = this.speakService.getVoiceList();
+    this.ref.detectChanges();
+  }
+
+  setLanguage(): void {
+    this.speakService.language = this.language;
+    console.log('Set Language to ' + this.language + '.');
+    this.getVoice();
+  }
+
+  setASR(): void {
+    this.listenService.asr = this.asr;
+    console.log('Set ASR to ' + this.asr + '.');
+  }
+
+  setTTS(): void {
+    this.speakService.tts = this.tts;
+    console.log('Set TTS to ' + this.tts + '.');
+    this.getVoice();
+  }
+
+  setVoice(): void {
+    this.speakService.voice = this.voice;
+    console.log('Set Voice to ' + this.voice + '.');
   }
 
 }
